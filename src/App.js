82bcfc015af484fd666a256efdda5a86b77deff3
Nikita <zaebumba@gmail.com>
2021-07-03 16:10:41 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    squares: this.createSquares(),
+    removed: [],
+  };
+
+  generateRandomColor() {
+    let r = Math.round((Math.random() * 255)); // red 0 to 255
+    let g = Math.round((Math.random() * 255)); // green 0 to 255
+    let b = Math.round((Math.random() * 255)); // blue 0 to 255
+
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  };
+
+  getColoredSquare(index) {
+    return <div key={index} style={{backgroundColor: this.generateRandomColor()}} onClick={() => this.removeSquare(index)}/>
+  };
+
+  getWhiteSquare(index) {
+    return <div key={index} style={{backgroundColor: 'white'}} onClick={() => this.removeSquare(index)}/>
+  };
+
+  createSquares() {
+    let squares = [];
+
+    for (let i = 0; i < 9; i ++) {
+      squares.push(this.getColoredSquare(i))
+    }
+
+    return squares
+  };
+
+  removeSquare = (index) => {
+    let { squares } = this.state;
+    let { removed } = this.state;
+
+    if (!removed.includes(index)) {
+      squares[index] = this.getWhiteSquare(index)
+      removed.push(index)
+    } else {
+      squares[index] = this.getColoredSquare(index)
+      this.setState({
+        removed: removed.filter(elem => elem !== index)
+      });
+    }
+
+    this.setState({
+      squares: squares,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, Oscar!</h1>
+        <hr />
+        <div className="box">{this.state.squares}</div>
+      </div>
+    )
+  };
 }
 
 export default App;
