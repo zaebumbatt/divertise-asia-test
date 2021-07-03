f1e1389a04ea3d3af1d0b2396e886f69da6cb75e
Nikita <zaebumba@gmail.com>
2021-07-03 16:50:19 +0700

    Change name

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, Oscar!</h1>
+        <h1>Hello, Mario!</h1>
         <hr />
         <div className="box">{this.state.squares}</div>
       </div>
